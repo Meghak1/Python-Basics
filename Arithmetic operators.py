7eaf392a6635f0d2788c6c94e2ded36708efa9c9
Meghaa K <140928258+Meghak1@users.noreply.github.com>
2025-12-31 12:22:22 +0530

    arithmetic operations in Arithmetic operators.py
    Refactored arithmetic operations for clarity and reusability.

---
+++ Arithmetic operators.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)     
+    print(a*b)
+
+
+#most best version
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    summation = a + b
+    difference = a - b
+    product = a * b
+
+    print(summation)
+    print(difference)
+    print(product)
